69fde8923773ca1172bc7e747f9a6668aa94b022
Valeria caro <valeringallego1@gmail.com>
2026-05-09 11:39:19 -0500

    chore: limpiar archivos y actualizar gitignore

--- backend/backend/app/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from app.models.base import Base
-from app.models.laboratorio import Laboratorio
-from app.models.horario import HorarioLaboratorio
-from app.models.reserva import RecursoLaboratorio, Reserva, AuditoriaReserva
